c7b5c9c110fcae4d9a8a7c342cd9823e6abfb598
bemadeveloper <akbermet1990@icloud.com>
2024-01-11 19:23:31 +0600

    Cleaning comments

--- ProjectForHomework/ViewController.swift
+++ ProjectForHomework/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  ProjectForHomework
-//
-//  Created by Bema on 11/1/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func setupView() {
